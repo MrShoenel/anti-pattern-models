ae52752354e8d2594e4298636f13ade564538199
Sebastian Hönel <sebastian.honel@lnu.se>
2021-01-19 11:17:58 +0100

    Make some changes to updated BTW models (not working)

--- models/BTW_updated.R
+++ models/BTW_updated.R
@@ -1,5 +1,5 @@
 
-M_updated <- function(theta_b_org, theta_b, r, f, num_samples = 1e3, zNormalize = FALSE) {
+M_updated <- function(theta_b_org, theta_b, r, f, num_samples = 1e3, zNormalize = FALSE, valueForZeroLenIntervals = NA_real_) {
   stopifnot(length(theta_b_org) == length(theta_b))
   stopifnot(is.function(r) && is.function(f))
   stopifnot(num_samples >= length(theta_b))
@@ -16,74 +16,63 @@ M_updated <- function(theta_b_org, theta_b, r, f, num_samples = 1e3, zNormalize 
     theta_b_org[b + 1] - theta_b_org[b]
   })
   
+  X_r <- range(theta_b_org)
+  x_r <- seq(from = X_r[1], to = X_r[2], length.out = num_samples)
+  Q <- seq_len(length.out = length(theta_l))
   
-  X <- seq(from = min(theta_b), to = max(theta_b), length.out = num_samples)
-  y <- Vectorize(r)(X)
-  
-  # Contrary to the pseudo-code, we will create a list of
-  # all intervals and scaled+translated functions to look
-  # them up, that's much faster.
-  
-  f_primes <- list()
-  for (iIdx in seq_len(length(theta_b) - 1)) {
-    f_primes[[paste0(iIdx)]] <- (function(q) {
+  y <- Vectorize(r)(x_r)
+  y_hat <- rep(NA_real_, num_samples)
+  q_idx <- rep(NA, num_samples)
+  X_q <- c()
+
+  i <- 1
+  for (q in Q) {
+    last_q <- q == rev(Q)[1]
+    b_os <- theta_b_org[q]
+    b_oe <- theta_b_org[q + 1]
+    b_qs <- theta_b[q]
+    b_qe <- theta_b[q + 1]
+    
+    x_r_supp <- if (last_q) {
+      x_r[x_r >= b_os & x_r <= b_oe]
+    } else {
+      x_r[x_r >= b_os & x_r < b_oe]
+    }
+    supp_len <- length(x_r_supp)
+    stopifnot(supp_len > 0)
+    
+    l_q <- theta_l[q]
+    phi_q <- if (q == 1) 0 else sum(theta_l[1:(q - 1)])
+    de_o <- theta_l_org[q]
+    
+    samp_func <- Vectorize(function(x) {
+      temp <- de_o * (x - s - phi_q) / l_q + b_os
+      f(temp)
+    })
+    
+    # Let's check the new query interval.
+    if (l_q == 0) {
+      # It was requested to have a length of 0, but we need to
+      # sample 'supp_len' number of elements, which we cannot.
+      # We repeat 'valueForZeroLenIntervals' this element instead.
+      y_hat[i:(i + supp_len - 1)] <- valueForZeroLenIntervals
+      X_q <- c(X_q, rep(b_qs, supp_len))
+    } else {
+      # An interval of negative length means that it goes backward.
+      # This case should be caught by the regularizers. However, we
+      # can still return some meaningful values, by sampling from
+      # the range of the interval, and then reversing the values.
       
-      lq <- if(theta_l[q] == 0) 1 else theta_l[q]
-      phi_q <- if (q == 1) 0 else sum(theta_l[1:(q - 1)])
-      b_os <- theta_b_org[q]
-      de_o <- theta_l_org[q]
+      # This also works if b_qs > b_qe ..
+      x_q <- seq(from = b_qs, to = b_qe, length.out = supp_len)
+      X_q <- c(X_q, x_q) # append support used (before reversing)
       
-      Vectorize(function(x) {
-        temp <- de_o * (x - s - phi_q) / lq + b_os
-        f(temp)
-      })
-    })(iIdx)
-  }
-  
-  
-  # Now we can sample from all intervals. Note that this
-  # Model stays in an interval for as long as it is valid.
-  # If a subsequent interval overlaps, it will not switch
-  # into it.
-  start <- min(theta_b)
-  end <- max(theta_b)
-  start_last <- theta_b[length(theta_b) - 1]
-  
-  determine_interval_for_x <- function(x) {
-    stopifnot(x >= start && x <= end)
-    
-    # Note that intervals are [start, end), except for the
-    # last interval, which is [start, end].
-    if (x >= start_last) {
-      return(length(theta_b) - 1)
+      y_hat[i:(i + supp_len - 1)] <- samp_func(x_r_supp)
     }
     
-    iIdx <- 1
-    for (bIdx in seq_len(length(theta_b) - 1)) {
-      if (x >= theta_b[bIdx] && x < theta_b[bIdx + 1]) {
-        break
-      } else {
-        iIdx <- iIdx + 1
-      }
-    }
+    q_idx[i:(i + supp_len - 1)] <- q
     
-    if (iIdx > (length(theta_b) - 1)) {
-      print(x)
-      print(theta_b)
-      warning("44!")
-      return("NA")
-    }
-    
-    paste0(iIdx)
-  }
-  
-  y_hat <- rep(NA, num_samples)
-  int_idx <- rep(NA, num_samples)
-  for (xIdx in seq_len(num_samples)) {
-    iIdx <- determine_interval_for_x(X[xIdx])
-    #y_hat[xIdx] <- f_primes[[iIdx]](X[xIdx])
-    y_hat[xIdx] <- if (iIdx == "NA") NA_real_ else f_primes[[iIdx]](X[xIdx])
-    int_idx[xIdx] <- iIdx
+    i <- i + supp_len
   }
   
   if (zNormalize) {
@@ -91,13 +80,12 @@ M_updated <- function(theta_b_org, theta_b, r, f, num_samples = 1e3, zNormalize 
     y_hat <- (y_hat - mean(y_hat)) / sd(y_hat)
   }
   
-  # TODO: We have currently allowed this - the error function needs to take care of this!
-  #stopifnot(!any(is.na(c(y, y_hat))))
   data.frame(
-    X = X, # return the support used
+    x_r = x_r,
+    X_q = X_q,
     y = y,
     y_hat = y_hat,
-    int_idx = factor(x = int_idx, levels = paste0(1:(length(theta_b) - 1)), ordered = TRUE)
+    q_idx = factor(x = q_idx, levels = paste0(1:length(theta_l)), ordered = TRUE)
   )
 }
 
@@ -111,31 +99,31 @@ L_updated_log <- function(
   loss <- 0
   res <- M_updated(theta_b_org = theta_b_org, theta_b = theta_b, r = r, f = f)
   
-  ######## KL
-  idx_not_NA <- !(is.na(res$y) | is.na(res$y_hat))
-  if (sum(idx_not_NA) == 0) {
-    stop("All data is NA.")
-  }
-  y_not_NA <- res$y[idx_not_NA]
-  y_hat_not_NA <- res$y_hat[idx_not_NA]
-
-  y_not_NA <- y_not_NA - min(y_not_NA)
-  y_not_NA <- y_not_NA / sum(y_not_NA)
-  y_hat_not_NA <- y_hat_not_NA - min(y_hat_not_NA)
-  y_hat_not_NA <- y_hat_not_NA / sum(y_hat_not_NA)
-
-  idx_not_0 <- !(y_not_NA == 0 | y_hat_not_NA == 0)
-  if (sum(idx_not_0) == 0) {
-    stop("All data is 0.")
-  }
-
-  y_not_NA <- y_not_NA[idx_not_0]
-  y_hat_not_NA <- y_hat_not_NA[idx_not_0]
-
-  dkl_pq <- sum(y_not_NA * log(y_not_NA / y_hat_not_NA))
-  dkl_qp <- sum(y_hat_not_NA * log(y_hat_not_NA / y_not_NA))
-
-  loss <- weightErr * (dkl_pq + dkl_qp)
+  # ######## KL
+  # idx_not_NA <- !(is.na(res$y) | is.na(res$y_hat))
+  # if (sum(idx_not_NA) == 0) {
+  #   stop("All data is NA.")
+  # }
+  # y_not_NA <- res$y[idx_not_NA]
+  # y_hat_not_NA <- res$y_hat[idx_not_NA]
+  # 
+  # y_not_NA <- y_not_NA - min(y_not_NA)
+  # y_not_NA <- y_not_NA / sum(y_not_NA)
+  # y_hat_not_NA <- y_hat_not_NA - min(y_hat_not_NA)
+  # y_hat_not_NA <- y_hat_not_NA / sum(y_hat_not_NA)
+  # 
+  # idx_not_0 <- !(y_not_NA == 0 | y_hat_not_NA == 0)
+  # if (sum(idx_not_0) == 0) {
+  #   stop("All data is 0.")
+  # }
+  # 
+  # y_not_NA <- y_not_NA[idx_not_0]
+  # y_hat_not_NA <- y_hat_not_NA[idx_not_0]
+  # 
+  # dkl_pq <- sum(y_not_NA * log(y_not_NA / y_hat_not_NA))
+  # dkl_qp <- sum(y_hat_not_NA * log(y_hat_not_NA / y_not_NA))
+  # 
+  # loss <- weightErr * (dkl_pq + dkl_qp)
   
   
   
@@ -146,11 +134,8 @@ L_updated_log <- function(
   
   
   ######## RSS
-  # loss <- loss + weightErr * log(1 + sum(na.omit(res$y - res$y_hat)^2))
-  
-  ######### RMSE
-  # numData <- sum(complete.cases(res$y_hat))
-  # loss <- loss + weightErr * log(1 + sum(na.omit(res$y - res$y_hat)^2))
+  numData <- sum(complete.cases(res$y_hat))
+  loss <- loss + weightErr * log(1 + sum(na.omit(res$y - res$y_hat)^2))
   
   loss
 }
